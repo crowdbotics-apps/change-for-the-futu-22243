bd33a4400b196e892f3a7c2fafd41c67f8db96a4
Crowdbotics <team@crowdbotics.com>
2020-11-03 03:03:47 +0000

    Update models

--- backend/change_for_the_futu_22243/wsgi.py
+++ backend/change_for_the_futu_22243/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'change_for_the_futu_22243.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "change_for_the_futu_22243.settings")
 
 application = get_wsgi_application()
